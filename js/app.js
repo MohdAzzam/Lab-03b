8f312bc45e5ad7cdb5150dbfc3ccf2157786ec92
MohdAzzam <48564462+MohdAzzam@users.noreply.github.com>
2021-01-27 15:16:22 +0200

    adding some js code to the project

---
+++ js/app.js
@@ -0,0 +1,52 @@
+
+var userName = prompt("Tell Us Your Name");
+
+if (userName != null) {
+  alert("Welcome " + userName);
+} else {
+  alert("Welcome user");
+}
+
+function myFunction() {
+  var s = confirm("Confirm it");
+  if (s == true) {
+    document.write(
+      '<h4 style="color:blue;">' +
+        "You Confirm the request " +
+        userName +
+        " !!" +
+        "</h4>" +
+        "<br>" +
+        '<a href="index.html">' +
+        "Back" +
+        "</a>"
+    );
+  } else {
+    document.write(
+      '<h4 style="color:red;">' +
+        "You Decline the request " +
+        userName +
+        " !!" +
+        "</h4>" +
+        "<br>" +
+        '<a href="index.html">' +
+        "Back" +
+        "</a>"
+    );
+  }
+}
+
+function Age() {
+  var age = prompt("Your Age");
+
+  if (age < 18) {
+    alert("Hello Teenger");
+  } else {
+    alert("Hello Adults");
+  }
+}
+function changeColor(){
+    var articleColor=prompt('in Which Color you want to see the Article');
+    document.getElementById("article").style.color = articleColor;
+
+}
